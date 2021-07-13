2cd6a1068805a359d398134008895734e3a47cfb
brendoprendo <brendan.prednis@gmail.com>
2021-07-13 15:14:54 -0700

    added notes on types and function types.

--- Typescript/Week-1/TypescriptEssentials/functions.ts
+++ Typescript/Week-1/TypescriptEssentials/functions.ts
@@ -11,4 +11,27 @@ function newGreeting(greet?: string): void {
     console.log(greet);
 }
 
-// 
+// fat arrow short syntax
+
+let squareit = (x: number) => x * x;
+
+let scores: number[] = [10, 20, 30, 40];
+
+let highScores: number[];
+
+highScores = scores.filter((el, i?, arr?) => {
+    if (el > 10) {
+        return true;
+    }
+});
+
+function logError(err: string): void {
+    console.error(err);
+}
+let score = scores[0];
+
+let logger: (value: string) => void;
+
+if (score < 0) {
+    logger = logError;
+}
